a73555aad5821a3914f87ba5d8f7e4bf6e7068ce
Shree Shrestha <sauravshree@hotmail.com>
2021-08-27 21:56:19 +0200

    added migration

---
+++ GreenFlux.SmartCharging.Infrastructure/Migrations/20210827195530_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GreenFlux.SmartCharging.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CapacityAmps = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargeStations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargeStations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargeStations_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connectors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChargeStationId = table.Column<int>(type: "int", nullable: false),
+                    MaxCurrentAmps = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connectors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Connectors_ChargeStations_ChargeStationId",
+                        column: x => x.ChargeStationId,
+                        principalTable: "ChargeStations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargeStations_GroupId",
+                table: "ChargeStations",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connectors_ChargeStationId",
+                table: "Connectors",
+                column: "ChargeStationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connectors");
+
+            migrationBuilder.DropTable(
+                name: "ChargeStations");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
