98359f6f64253888e9fce61551f807ef4e5d16a6
Shree Shrestha <sauravshree@hotmail.com>
2021-08-28 03:15:15 +0200

    added TODO comment

--- src/GreenFlux.SmartCharging.Infrastructure/DependencyInjection.cs
+++ src/GreenFlux.SmartCharging.Infrastructure/DependencyInjection.cs
@@ -11,7 +11,7 @@ namespace GreenFlux.SmartCharging.Infrastructure
         public static IServiceCollection AddSmartChargingData(this IServiceCollection services, IConfiguration configuration)
         {
             string dbConnectionString = configuration.GetConnectionString("SmartChargingDbContext");
-            //TODO: dbConnectionString is null. I didn't have time to look into it. 
+            //TODO: dbConnectionString is null somehow. I didn't have time to look into it. 
             services.AddDbContext<SmartChargingDbContext>(options =>
                 options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SmartCharging;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
             );
